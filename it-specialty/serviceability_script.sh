7b3968d2a88d682ee22961efb80ecc4cf94f167e
Maksim Morozov <maksmorozman@gmail.com>
2022-03-25 12:26:02 +0300

    update: rebase all the commits from workflows
    fix: minor updates of syntax
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    fix: minor updates
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    update: identification of some issues
    
    My test branch (#3)
    
    * update: identification of some issues
    
    * fix: branch name into java_app_ci_cd.yml

---
+++ it-specialty/serviceability_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CODE=$(curl -s --output /dev/null \
+    --write-out '%{http_code}' \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d '{"name": "maintenance"}' \
+    http://localhost:8080/api/v1/specialty)
+
+echo "$CODE"
+
+if [[ "$CODE" != "200" ]]
+then
+    echo "FAILURE HTTP RETURN CODE!"
+else
+    echo "200 HTTP RETURN CODE, SUCCESS!"
+fi

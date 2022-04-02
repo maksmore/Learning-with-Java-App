0cff681486f44c8d722049a18c66137ddf09127a
Maksim Morozov <maksmorozman@gmail.com>
2022-04-02 21:03:46 +0300

    update: identification of some issues

--- it-specialty/serviceability_script.sh
+++ it-specialty/serviceability_script.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
-CODE=$(curl -s --output /dev/null \
---write-out '%{http_code}' \
--H "Content-Type: application/json" \
+# --output /dev/null \
+# --write-out '%{http_code}'
+CODE=$(curl -s -H "Content-Type: application/json" \
 -X POST \
 -d '{"name": "maintenance"}' \
 http://localhost:8080/api/v1/specialty)
 
 echo "$CODE"
-if [[ "$CODE" != "200" ]]
-then
-    echo "FAILURE HTTP RETURN CODE!"
-else
-    echo "200 HTTP RETURN CODE, SUCCESS!"
-fi
+# if [[ "$CODE" != "200" ]]
+# then
+#     echo "FAILURE HTTP RETURN CODE!"
+# else
+#     echo "200 HTTP RETURN CODE, SUCCESS!"
+# fi

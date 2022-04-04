29bb40f9746c3930d52da09a7d73a91f2bd6af27
Maksim Morozov <maksmorozman@gmail.com>
2022-04-04 10:33:32 +0300

    update: minor feature updating (#7)

--- it-specialty/src/main/java/by/morozov/its/controller/SpecialtyController.java
+++ it-specialty/src/main/java/by/morozov/its/controller/SpecialtyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class SpecialtyController {
 
     private final SpecialtyService specialtyService;
-    private static final String INFO = "It is my test Java application!";
+    private static final String INFO = "It is my another test Java application!";
 
     @PostMapping
     public Specialty getSpecialtyByName(@RequestBody SpecialtyRequestDto request) {
